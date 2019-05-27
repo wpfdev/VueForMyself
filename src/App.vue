c51629ac50028963cad56a993c44c4ccaf984d27
AndreyS <vloac2013@ya.ru>
2019-05-27 18:50:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <app-car></app-car>
+  </div>
+</template>
+
+<script>
+import Car from "./Components/Car.vue";
+
+export default {
+  data() {
+    return {
+      msg: "Welcome to Vue"
+    };
+  },
+  components: {
+    appCar: Car
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/Components/Car.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ carName }}</h3>
+    <p>Year: {{carYear}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carName: "Ford",
+      carYear: 2019
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
